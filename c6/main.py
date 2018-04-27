8d7b9f6e72ff56dbfbdb3ac64b08d21a1d2c8682
Diego Fernández <diego130892@gmail.com>
2018-04-27 20:09:53 +0200

    first tyr 8

--- c6/main.py
+++ c6/main.py
@@ -1,5 +1,24 @@
+
 def parser_note(line):
     X,L,S,P=list(map(float,line.strip().split()))
-    time_period_punt=((X-L)/S,(X)/S,P)
+    time_period_punt=((X)/S,(X+L)/S,P)
     return time_period_punt
+
+def select(notes):
+    
+    notes.sort(key=lambda x:(x[0],-x[2]))
+    print(notes)
+    last=-float('inf')
+    for i in notes:
+        if last<i[0]:
+            yield i[-1]
+            last=i[1]
+
+def greedy_main(s):
+    L=list(map(parser_note,s.split('\n')
+            ))
+
+    return list(select(L))
+
+
 
